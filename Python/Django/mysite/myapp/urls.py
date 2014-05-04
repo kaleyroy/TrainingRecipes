894deaae8029861dd0ba025ecb137ecf04db85f7
kaleylab <kaleylab@gmail.com>
2014-05-04 18:26:27 +0800

    Django: 2nd section

--- Python/Django/mysite/myapp/urls.py
+++ Python/Django/mysite/myapp/urls.py
@@ -2,19 +2,19 @@ from django.conf.urls import patterns, include, url
 from myapp.views import hello
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myapp.views.home', name='home'),
     # url(r'^myapp/', include('myapp.foo.urls')),
 
-	('^hello/', hello),
+    ('^hello/', hello),
 	
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
