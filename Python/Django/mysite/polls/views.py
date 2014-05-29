05b6eee97ad7d26c85ed3fe359d222e727403bbe
kaleylab <kaleylab@gmail.com>
2014-05-29 18:03:20 +0800

    Django:Completed Training.

--- Python/Django/mysite/polls/views.py
+++ Python/Django/mysite/polls/views.py
@@ -1,12 +1,13 @@
 # Create your views here.
-from django.http import HttpResponse,Http404
+from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
 
-from polls.models import Poll
-
+from polls.models import Poll,Choice
+'''
 def index(request):
-    lastest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    context = {'lastest_poll_list': lastest_poll_list }
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list }
     return render(request, 'polls/index.html', context)
 
 def detail(request,poll_id):
@@ -14,7 +15,17 @@ def detail(request,poll_id):
     return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request,poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
-
+    poll = get_object_or_404(Poll, pk = poll_id)
+    return render(request,'polls/results.html',{'poll': poll})
+'''
 def vote(request,poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        context = {'poll': p,'error_message': "You didn't select a choice."}
+        return render(request, 'polls/detail.html', context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
